3dc3f1c8cf3761eca8285ad6d56657435fb36014
Muhammad Haris Khan <harisniazi302@gmail.com>
2023-07-20 18:54:39 +0500

    Call Explorer

--- cypress/Integration/AdepsiUIAutomation/Login.js
+++ cypress/Integration/AdepsiUIAutomation/Login.js
@@ -4,10 +4,11 @@ describe('Login Module of Adepsi', () => {
         cy.visit("https://demo.adepsi.cloud/") 
     });
     it('Valid Login Credentials-Login must be Successful ', () => {
-        cy.viewport(2150,1075)
+        cy.viewport(2150,1075);
         cy.visitLoginPage();
         cy.get('#mat-input-0').type("demo.admin@adepsi.cloud")
         cy.get('#mat-input-1').type('Ye3&M57g%kH?B3E$')
         cy.get('.mat-ripple.app-button.font-bold').click();
     });
+    
 });
