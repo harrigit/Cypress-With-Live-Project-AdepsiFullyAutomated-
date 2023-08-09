e13f0a1741986993ef4937b5f65eb9c0c955d51e
Muhammad Haris Khan <harisniazi302@gmail.com>
2023-08-09 18:04:16 +0500

    User Management Update/Delete User

--- cypress/support/UserManagement.js
+++ cypress/support/UserManagement.js
@@ -30,8 +30,28 @@ Cypress.Commands.add("CreateUser", () => {
 
 Cypress.Commands.add("updateUser",()=>{
     //pending
+    cy.get(".material-icons.arrow-rit.ng-star-inserted").click();
+    cy.get('[routerlink="/home/settings/user-management"]').click();
+    cy.wait(6000);
+    
+    cy.get(':nth-child(1) > :nth-child(7) > div > .mat-menu-trigger > .mat-icon').click();
+    cy.get('.menu > :nth-child(2) > .mat-ripple').click();
+    cy.wait(1000);
+    cy.get(':nth-child(1) > .form-control').click().type("UpdatedUser");
+    cy.wait(1000);
+    cy.get('.add-edit-btn > .mat-ripple').click();
+    cy.wait(3000);
 
 })
 Cypress.Commands.add("deleteUser",()=>{
 //pending
+cy.get(".material-icons.arrow-rit.ng-star-inserted").click();
+cy.get('[routerlink="/home/settings/user-management"]').click();
+cy.wait(6000);
+
+cy.get(':nth-child(1) > :nth-child(7) > div > .mat-menu-trigger > .mat-icon').click();
+cy.get('.menu > :nth-child(3) > .mat-ripple').click();
+cy.wait(1000);
+cy.get('.action-btn-bg').click();
+cy.wait(5000);
 })
