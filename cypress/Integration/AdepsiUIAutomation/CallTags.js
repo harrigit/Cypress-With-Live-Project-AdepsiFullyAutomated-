bc26fc84171a25ad68f0d63b03f25e392b6ea47b
Muhammad Haris Khan <harisniazi302@gmail.com>
2023-08-10 12:41:20 +0500

    Test Cases with Videos

--- cypress/Integration/AdepsiUIAutomation/CallTags.js
+++ cypress/Integration/AdepsiUIAutomation/CallTags.js
@@ -1,24 +1,22 @@
-describe(' Check Call Tags ', () => {
-    it('User Must Go To  Create Tag Page And  Create A Tags With unique Name', () => {
-        cy.LoginPage();
-        cy.LoginInput();
-        cy.wait(4000);
+describe(" Check Call Tags ", () => {
+  it("User Must Go To  Create Tag Page And  Create A Tags With unique Name", () => {
+    cy.LoginPage();
+    cy.LoginInput();
+    cy.wait(4000);
 
+    cy.callTagsCreate();
+  });
+  it("user Must Update A tag", () => {
+    cy.LoginPage();
+    cy.LoginInput();
+    cy.wait(4000);
 
-        cy.callTagsCreate();
-    });
-    it('user Must Update A tag', () => {
-        cy.LoginPage();
-        cy.LoginInput();
-        cy.wait(4000);
-
-cy.updatetag();
-        
-    });
-    it('User Must Delete A tag', () => {
-        cy.LoginPage();
-        cy.LoginInput();
-        cy.wait(4000);
-        cy.deleteTag();
-    });
+    cy.updatetag();
+  });
+  it("User Must Delete A tag", () => {
+    cy.LoginPage();
+    cy.LoginInput();
+    cy.wait(4000);
+    cy.deleteTag();
+  });
 });
