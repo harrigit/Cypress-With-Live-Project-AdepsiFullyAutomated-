157ad0f0a295ba0fa8698c4f15bdfe8238a90d55
Muhammad Haris Khan <harisniazi302@gmail.com>
2023-08-21 15:09:47 +0500

    No Chnages

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  pageLoadTimeout: 60000,
 
-  pageLoadTimeout: 60000, 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-     
     },
     //Telling the path where Our Files are Located
-    specPattern: 'cypress/Integration/AdepsiAutomation/*.js'
-      
+    specPattern: "cypress/Integration/AdepsiAutomation/*.js",
   },
+
   
 });
